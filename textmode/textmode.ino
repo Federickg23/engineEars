326d245c6834e12234452a1e1a94cb738c0dd6d7
joosthemoos <jkuleshov2@gmail.com>
2019-04-26 12:57:15 -0400

    Final product

--- textmode/textmode.ino
+++ textmode/textmode.ino
@@ -27,41 +27,144 @@
 #define RA8875_CS 10
 #define RA8875_RESET 9
 
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
 Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET);
 uint16_t tx, ty;
-
 
 const int buttonPin1 = 4;     // the number of the pushbutton pin
 const int buttonPin2 = 5;     // the number of the pushbutton pin
 const int buttonPin3 = 6;     // the number of the pushbutton pin
 const int buttonPin4 = 7;     // the number of the pushbutton pin
 
-int buttonState1 = LOW;
-int buttonState2 = LOW;
-int buttonState3 = LOW;
-int buttonState4 = LOW;
-int lastButtonState1 = LOW;   
-int lastButtonState2 = LOW;   
-int lastButtonState3 = LOW;   
-int lastButtonState4 = LOW;   
+const int speaker = 2;
+const int pitches[] = { 1, 2, 3, 4, 5 };
+const int tones[] = { 100, 200, 300, 400 };
 
-bool isPushed1 = false;
-bool isPushed2 = false;
-bool isPushed3 = false;
-bool isPushed4 = false;
+//Not currently using these
+volatile int buttonState1 = LOW;
+volatile int buttonState2 = LOW;
+volatile int buttonState3 = LOW;
+volatile int buttonState4 = LOW;
+//int lastButtonState1 = LOW;   
+//int lastButtonState2 = LOW;   
+//int lastButtonState3 = LOW;   
+//int lastButtonState4 = LOW;   
+//////////////////////////
+
+//bool isPushed1 = false;
+//bool isPushed2 = false;
+//bool isPushed3 = false;
+//bool isPushed4 = false;
+
+int randomNum = 0;
+int xPos = 20;
+int val = 19;
+int width = 150;
+int playerY = tft.height();
+int max  = 0;
 
 void setup()
 {
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  pinMode(buttonPin3, INPUT);
-  pinMode(buttonPin4, INPUT);
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(buttonPin3, INPUT_PULLUP);
+  pinMode(buttonPin4, INPUT_PULLUP);
+  pinMode (speaker,OUTPUT);
   digitalWrite(buttonPin1, LOW);
   digitalWrite(buttonPin2, LOW);
   digitalWrite(buttonPin3, LOW);
   digitalWrite(buttonPin4, LOW);
   Serial.begin(9600);
-  Serial.println("RA8875 start");
+  Serial.println("RA8875 starts");
 
   /* Initialize the display using 'RA8875_480x80', 'RA8875_480x128', 'RA8875_480x272' or 'RA8875_800x480' */
   if (!tft.begin(RA8875_800x480)) {
@@ -74,86 +177,114 @@ void setup()
   tft.PWM1config(true, RA8875_PWM_CLK_DIV1024); // PWM output for backlight
   tft.PWM1out(255);
   tft.fillScreen(RA8875_BLACK);
-
-  /* Switch to text mode */
   tft.textMode();
-  tft.cursorBlink(32);
 
-
-  /* Set a solid for + bg color ... */
-
-  /* ... or a fore color plus a transparent background */
-
-
-  /* Set the cursor location (in pixels) */
-  tft.textSetCursor(10, 10);
-
-  /* Render some text! */
-  char string[15] = "Hello, World! ";
-  tft.textTransparent(RA8875_WHITE);
-  tft.textWrite(string);
-  tft.textColor(RA8875_WHITE, RA8875_RED);
-  tft.textWrite(string);
-  tft.textTransparent(RA8875_CYAN);
-  tft.textWrite(string);
-  tft.textTransparent(RA8875_GREEN);
-  tft.textWrite(string);
-  tft.textColor(RA8875_YELLOW, RA8875_CYAN);
-  tft.textWrite(string);
-  tft.textColor(RA8875_BLACK, RA8875_MAGENTA);
-  tft.textWrite(string);
-
-  /* Change the cursor location and color ... */
-  tft.textSetCursor(100, 100);
-  tft.textTransparent(RA8875_RED);
-  /* If necessary, enlarge the font */
-  tft.textEnlarge(1);
-  /* ... and render some more text! */
-  tft.textWrite(string);
-  tft.textSetCursor(100, 150);
-  tft.textEnlarge(2);
-  tft.textWrite(string);
 }
 
 void loop()
 {
+//  Serial.println(val);
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3);
+  buttonState4 = digitalRead(buttonPin4);
 
-  tft.textMode();
-  if(digitalRead(buttonPin1)== HIGH){
-    isPushed1 = true;
-  }
-  if(digitalRead(buttonPin2)== HIGH){
-    isPushed2 = true;
-  }
-  if(digitalRead(buttonPin3)== HIGH){
-    isPushed3 = true;
-  }
-  if(digitalRead(buttonPin4)== HIGH){
-    isPushed4 = true;
-  }
-  if (isPushed1) {
-    tft.textWrite("Red ");
-    isPushed1 = false;
-    delay(50);
+  if(buttonState1 == HIGH){
+    Serial.println("Red is pressed");         
     
   }
-  else if (isPushed2) {
+  else{
+    Serial.println("Red is not pressed.");
+  }
+  if(buttonState2 == HIGH){
+    Serial.println("Blue is pressed");
+  }
+  else{
+    Serial.println("Blue is not pressed.");
+  }
+  if(buttonState3 == HIGH){
+    Serial.println("Green is pressed");
+  }
+  else{
+    Serial.println("Green is not pressed.");
+  }
+  if(buttonState4 == HIGH){
+    Serial.println("Yellow is pressed");
+  }
+  else{
+    Serial.println("Yellow is not pressed.");
+  }
     
-    tft.textWrite("Blue ");
-    isPushed2 = false;
-    delay(50);
-  }
-  else if (isPushed3) {
-    tft.textWrite("Green");
-    isPushed3 = false;
-    delay(50);
-  }
-  else if (isPushed4) {
-    tft.textWrite("Yellow");
-    isPushed4 = false;
-    delay(50);
-  }
+    if(val++ == 20){
+      width = 150;
+      randomNum = random(4);
+      Serial.println("randomNum");
+      Serial.println(randomNum);
+      val = 0;
+      xPos = 200;
+      playTone(tones[randomNum], 500);
+      drawBlock(randomNum, xPos, width);
+      drawPlayer(playerY);
+      playerY = tft.height();
+      max = tft.height()- 150*randomNum;
+    }
+    else{
+      Serial.println("playerY");
+      Serial.println(playerY);
+      if(val <=10){
+        playerY = playerY - max/10;
+        drawPlayer(playerY);
+      }\
+      else{
+        playerY = playerY + max/10;
+        drawPlayer(playerY); 
+      }
+      xPos-=20;
+      if(xPos > 21){
+        
+      drawBlock(randomNum, xPos, width);
+      }
+      else if(val < 16){
+        width = width -20;
+        if(width <=0){
+           width = 0;
+        }
+        drawBlock(randomNum, 0, width);
+      }
+//      drawPlayer(playerY);
+    }
+    
+  buttonState1 = LOW;
+  buttonState2 = LOW;
+  buttonState3 = LOW;
+  buttonState4 = LOW;
 
-  
-  
+    delay(100); 
+    
+}
+
+void drawBlock(int multiplier, int position, int width){
+//  tft.fillScreen(RA8875_BLACK);
+  if(multiplier != 0){
+    tft.fillRect(position, tft.height(), width, 150*multiplier+20, RA8875_RED);
+  }
+}
+
+void drawPlayer(int jump){
+  tft.fillScreen(RA8875_BLACK);
+  tft.fillCircle(50, jump-100, 10, RA8875_WHITE);
+  tft.drawLine(50, jump-100, 50, jump-50, RA8875_WHITE);//body
+  tft.drawLine(50, jump-80, 25, jump-90, RA8875_WHITE);//left arm
+  tft.drawLine(50, jump-80, 75, jump-90, RA8875_WHITE);//right arm
+  tft.drawLine(50, jump-50, 25, jump-40, RA8875_WHITE);//left leg
+  tft.drawLine(50, jump-50, 75, jump-40, RA8875_WHITE);//right leg
+}
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speaker, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speaker, LOW);
+    delayMicroseconds(tone);
+  }
 }
